00eb5188973c50201340915aeb2242ce7eb3257c
filippawlicki <filip.pawlicki77@gmail.com>
2026-03-08 21:50:03 +0100

    Add pins config

---
+++ include/config.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#define PIN_RGB 16
+
+// Switches
+#define PIN_SW1 26
+#define PIN_SW2 14
+#define PIN_SW3 6
+#define PIN_SW4 8
+#define PIN_SW5 15
+#define PIN_SW6 7
+#define NUM_OF_SW 6
+
+// Encoder
+#define PIN_ENC_A 28
+#define PIN_ENC_B 29
+#define PIN_ENC_SW 27
+
+// OLED
+#define PIN_OLED_SDA 0
+#define PIN_OLED_SCL 1
